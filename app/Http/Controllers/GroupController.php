03c655132173e829b6aeed0f69bc1ca250121080
RomuloLim <kinggbr0102@gmail.com>
2022-08-29 00:04:23 -0300

    wip

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Team;
-use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Auth;
 
 class GroupController extends Controller
@@ -11,25 +10,26 @@ class GroupController extends Controller
     public function draw()
     {
         $groupsNumber = 2;
+        $modalityId = Auth::user()->modality_id;
         $randomTeams = Team::where('modality', Auth::user()->modality_id)->get()->shuffle();
         $teamsCount = $randomTeams->count();
-        $modalityId = Auth::user()->modality_id;
 
-        $teamsGroupped = [];
+        $teamsGrouped = [];
         $index = 0;
         $groupIndex = 1;
 
         while ($index < ($teamsCount/$groupsNumber) * $groupIndex){
-            $teamsGroupped[$index]['team_id'] = $randomTeams->get($index)->id;
-            $teamsGroupped[$index]['modality_id'] = $modalityId;
-            $teamsGroupped[$index]['group_number'] = $groupIndex;
+            $teamsGrouped[$index] = [
+                'team_id' => $randomTeams->get($index)->id,
+                'modality_id' => $modalityId,
+                'group_number' => $groupIndex,
+            ];
 
             $index++;
-
             if ($index >= ($teamsCount/$groupsNumber) * $groupIndex && $groupIndex < $groupsNumber)
                     $groupIndex++;
         }
 
-        dd($teamsGroupped);
+        dd($teamsGrouped);
     }
 }
