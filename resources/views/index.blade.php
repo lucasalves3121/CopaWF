9bac6eb21cede1f0ede054df849b22f996ef8834
lucasalves3121 <lucasalves3121@gmail.com>
2022-09-05 15:40:53 -0300

    first commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
    
 
     <!-- Header Start -->
-    <div class="jumbotro jumbotron-fluid position-relative overlay-bottom" style="margin-bottom: -50px;">
+    <div class="jumbotro jumbotron-fluid position-relative overlay-bottom" style="margin-bottom: 20px;">
         <div class="contai">      
              
             <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
