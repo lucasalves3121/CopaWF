f8d248732b5114c7a5f89f47ad5f66690ec1e775
RomuloLim <kinggbr0102@gmail.com>
2022-09-11 21:46:07 -0300

    resta fazer checagens de repetição nos grupos

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use function GuzzleHttp\Promise\all;
 
 class GroupController extends Controller
 {
@@ -60,7 +61,9 @@ class GroupController extends Controller
 
         $allGames = collect();
 
-        $roundCount = 0;
+        $roundCount = 1;
+
+        $gamesPerRound = (int) floor($teams->count()/2);
 
         foreach ($teams as $teamOne){
             foreach ($teams as $teamTwo){
@@ -74,13 +77,65 @@ class GroupController extends Controller
             }
         }
 
+        $allGames = $allGames->shuffle()->toArray();
+
+        $i = 0;
+        $gameShuffle = [];
+        $outOfRound = [];
+        $outOfLatest = null;
+
+        while($i < count($allGames)){
+            if (!isset($gameShuffle[$roundCount])){
+                $gameShuffle[$roundCount][] = $allGames[$i];
+            }else{
+                $teamOut = $this->checkOutOfRound($gameShuffle[$roundCount], $allGames[$i]);
+                if (!$teamOut){
+                    //TODO: add team in the next round and pop index from outOfRound array
+                    $gameShuffle[$roundCount][] = $allGames[$i];
+                }else{
+                    $outOfRound[] = $allGames[$i];
+                    $outOfLatest = $teamOut;
+                }
+            }
+
+            if (count($gameShuffle[$roundCount]) >= $gamesPerRound)
+                $roundCount++;
+
+            $i++;
+        }
+            dd([
+                'out' => $outOfRound,
+                'shuffle' => $gameShuffle
+                ]);
+
+
         /* condições para trocar a rodada
-         * 1 -> um time não pode duas vezes na mesma rodada
+         * 1 -> um time não pode jogar duas vezes na mesma rodada ok
          * 2 -> no máximo 1 time poderá ficar de fora da rodada
          * 3 -> um time só pode ficar fora uma vez
          * */
 
+        // total de jogos em uma rodada -> times/2 arredondado para baixo
 
-        dd($allGames->shuffle());
+
+    }
+
+    /**
+     * @param $roundArray
+     * @param $game
+     * @return bool
+     */
+    public function checkOutOfRound($roundArray, $game): int|bool
+    {
+        $firstTeamIds = Arr::pluck($roundArray, 'first_team');
+        $secondTeamIds = Arr::pluck($roundArray, 'second_team');
+
+        if (in_array($game['first_team'], $firstTeamIds) || in_array($game['second_team'], $firstTeamIds))
+            return $game['first_team'];
+        elseif(in_array($game['first_team'], $secondTeamIds) || in_array($game['second_team'], $secondTeamIds))
+            return $game['second_team'];
+
+
+        return false;
     }
 }
