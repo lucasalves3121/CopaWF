a12bacd1221f4806fa73946d3021f674395cd47a
lucasalves3121 <lucasalves3121@gmail.com>
2022-09-20 07:27:33 -0300

    first commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,26 +4,6 @@
 
 @section("content")
 
-<script type="text/javascript">
-    $(window).on('load',function(){
-        $('#myModal').modal('show');
-    });
-</script>
-
-
-<div class="modal hide fade" id="myModal">
-  <div class="modal-header">
-    <a class="close" data-dismiss="modal">×</a>
-    <h3>Modal header</h3>
-  </div>
-  <div class="modal-body">
-    <p>One fine body…</p>
-  </div>
-  <div class="modal-footer">
-    <a href="#" class="btn">Close</a>
-    <a href="#" class="btn btn-primary">Save changes</a>
-  </div>
-</div>
 
 
     <!-- Header Start -->
